65b86a47427f3d56c4b3e1a250af6fdea01c28eb
brunoaranda <brunoaranda@373abe66-34a0-11de-82fb-7f665ef830f7>
2009-04-29 11:06:34 +0000

    Setup and basic implementation that works

---
+++ trunk/psiscore-webservice/src/main/java/org/hupo/psi/mi/psiscore/ws/config/PsiscoreConfig.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2008 The European Bioinformatics Institute, and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hupo.psi.mi.psiscore.ws.config;
+
+/**
+ * Place-holder for the configuration. Initialized by Spring.
+ *
+ * @author Bruno Aranda (baranda@ebi.ac.uk)
+ * @version $Id: PsicquicConfig.java 32 2009-01-19 11:35:16Z brunoaranda $
+ */
+public class PsiscoreConfig {
+
+    private String groupId;
+    private String artifactId;
+    private String version;
+
+    public PsiscoreConfig() {
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
